c49df835666c8aedcfeda2d5c4ee00cbc988f99a
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-07 13:00:45 -0400

    Initial commit, basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import wallCandles from "./img/wall_candles.gif"
+
+export default function App(){
+    return (
+        <main>
+            <h1 id="main-title">Speed Typing</h1>
+
+            
+            <div className="centering">
+                <img src={wallCandles} />
+                <textarea rows={10} cols={20} className="font-medium"></textarea>
+                <img src={wallCandles} />
+            </div>
+            
+            <h1 className="font-medium">Time Remaining: __</h1>
+            
+            <div className="centering">
+                <button id="start-button" className="centering font-medium shadow">BEGIN</button>
+            </div>
+        </main>
+    )
 }
-
-export default App;
