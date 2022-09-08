8791d8ea218b600c7f853fda7fb831d9ee160e03
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-08 12:13:02 -0400

    MVP ready

--- src/App.js
+++ src/App.js
@@ -5,23 +5,30 @@ import wallCandles from "./img/wall_candles.gif"
 const GAME_LENGTH = 5
 
 export default function App(){
+    const ref = React.useRef(null)
     const [isGameRunning, setIsGameRunning] = React.useState(false)
     const [textValue, setTextValue] = React.useState("")
     const [timeRemaining, setTimeRemaining] = React.useState(0)
+    const [wordCount, setWordCount] = React.useState(0)
 
     function handleChange(event){
         setTextValue(event.target.value)
     }
 
-    function getWordCount(){
-        let count = textValue.trim().split(" ").length
-        console.log(count)
-        // return count
-    }
-
     function startGame(){
         setIsGameRunning(true)
+        setTextValue("")
         setTimeRemaining(GAME_LENGTH)
+        setWordCount(0)
+        ref.current.disabled = false
+        ref.current.focus()
+    }
+
+    function endGame(){
+        setIsGameRunning(false)
+        setWordCount(() => {
+            return textValue.trim().split(" ").filter(word => word !== "").length
+        })
     }
 
     React.useEffect(() => {
@@ -29,27 +36,39 @@ export default function App(){
             if (timeRemaining > 0)
                 setTimeRemaining(prevTime => prevTime-1)
             else
-                setIsGameRunning(false)
+                endGame()
         }, 1000)
         return () => clearTimeout(timer)
     }, [timeRemaining])
+
+
+
 
     return (
         <main>
             <h1 id="main-title">Speed Typing</h1>
 
-            
             <div className="centering">
                 <img src={wallCandles} alt="Wall Candles" />
                 <div id="parchment" className="centering">
-                    <textarea cols={18} value={textValue} onChange={handleChange} />
+                    <textarea
+                        id="main-text-area"
+                        ref={ref}
+                        cols={18}
+                        value={textValue} 
+                        onChange={handleChange} 
+                        disabled={!isGameRunning}
+                        placeholder={!isGameRunning ? "Click begin and start typing!" : ""}
+                    />
                 </div>
                 <img src={wallCandles} alt="Wall Candles" />
             </div>
+
             <br/>
+            
             <div id="hud">
                 <h1 className="font-medium">Time Remaining: {timeRemaining}</h1>
-                <h1 className="font-medium">Word Count: ___</h1>
+                <h1 className="font-medium">Word Count: {wordCount > 0 ? wordCount : "__"}</h1>
             </div>
             
             <div className="centering">
